21e00efdb030710e840fa2e40d669ece466ca7eb
HAL21 <struligt@gmail.com>
2024-04-27 13:21:04 +0200

    2. Basic html and css.

--- src/App.js
+++ src/App.js
@@ -3,21 +3,21 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style = {{
+        margin: 'auto',
+        width: 800,
+        paddingTop: "lrem"
+      }}>
+      <h1 className='title' >Pokemon Search</h1>
+      <table width="100%">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Type</th>
+          </tr>
+        </thead>
+      </table>
     </div>
   );
 }
