5f730b8a888dedec922f1f818a1a28f314024b32
HAL21 <struligt@gmail.com>
2024-04-27 21:00:38 +0200

    8. Component library use eg Material UI.

--- src/App.js
+++ src/App.js
@@ -3,13 +3,20 @@ import logo from './logo.svg';
 import './App.css';
 import PropTypes from 'prop-types';
 import styled from "@emotion/styled";
+import Button from "@mui/material/Button";
 
 const PokemonRow = ({pokemon, onSelect}) => (
   <tr>
     <td>{pokemon.name.english}</td>
     <td>{pokemon.type.join(", ")}</td>
     <td>
-      <button onClick={() => onSelect(pokemon)}>Select</button>
+      <Button 
+        variant="contained"
+        color="primary"
+        onClick={() => onSelect(pokemon)}
+      >
+        Select
+      </Button>
     </td>
   </tr>
 );
